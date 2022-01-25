50c6fb51d14bc01dc77691acbc5e3a3fa10b5a84
VictorPorlan <victorporlaninformatica@gmail.com>
2022-01-25 16:22:37 +0100

    docs: :memo: añado una explicación, sin cambios en codigo

--- domain/item/indestructible/indestructible.js
+++ domain/item/indestructible/indestructible.js
@@ -7,6 +7,10 @@ function Indestructible(nombre, precio, demanda, calidad, cantidad, id) {
 
 Indestructible.prototype = Object.create(protoItem)
 
+/**
+ * Aquí hago solo un return para hacer shadowing a la función usarItem y que no haga nada, esto es intencional.
+ * Los objetos indestructibles NO pierden durabilidad al usarlos, ese es el motivo de este return.
+ */
 Indestructible.prototype.usarItem = function () {
     return
 }
